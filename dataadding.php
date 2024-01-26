2aae09bb45e06cce8c22857ef29b9311f48ebf18
master-ginger <bhavanapillai.100@gmail.com>
2024-01-26 14:40:26 +0530

    Fix bug preventing users from submitting the subscribe form

---
+++ dataadding.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST['sub'])){
+$conn=mysqli_connect("localhost","root","43Malu@21","internship");
+$name=$_POST['name'];
+$email=$_POST['email'];
+$job=$_POST['job'];
+$descr=$_POST['d'];
+$filename= $_FILES['uploadfile']['name'];
+$tempname= $_FILES['uploadfile']['tmp_name'];
+$folder="internship/images/".$filename;
+move_uploaded_file($tempname,$folder);
+$q="insert into register values('$name','$email','$job','$descr')";
+mysqli_query($conn,$q);
+}   
+$query="select * from register";
+
+if($query){
+?>
+<h1>Data added</h1>
+<a href="dataconn.php">Go to home page</a>
+
+<?php
+}
+
+?>
