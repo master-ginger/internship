c9edb1818f16ef4dbcf8e1f51c29aafb0968ad58
Pillai Bhavana Radhakrishna <118704821+master-ginger@users.noreply.github.com>
2024-01-26 14:49:35 +0530

    Update dataadding.php

--- dataadding.php
+++ dataadding.php
@@ -5,10 +5,10 @@ $name=$_POST['name'];
 $email=$_POST['email'];
 $job=$_POST['job'];
 $descr=$_POST['d'];
-$filename= $_FILES['uploadfile']['name'];
-$tempname= $_FILES['uploadfile']['tmp_name'];
-$folder="internship/images/".$filename;
-move_uploaded_file($tempname,$folder);
+// $filename= $_FILES['uploadfile']['name'];
+// $tempname= $_FILES['uploadfile']['tmp_name'];
+// $folder="internship/images/".$filename;
+// move_uploaded_file($tempname,$folder);
 $q="insert into register values('$name','$email','$job','$descr')";
 mysqli_query($conn,$q);
 }   
